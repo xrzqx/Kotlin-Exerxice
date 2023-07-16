e4be7b4c35db8cb14f5ee026403c7aaba2826335
xrzqx <abdrzq84@gmail.com>
2023-07-17 01:39:50 +0700

    add to content

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposableInfoCard("Android","test", Color.Cyan)
+                    ComposeQuadrantApp()
                 }
             }
         }
