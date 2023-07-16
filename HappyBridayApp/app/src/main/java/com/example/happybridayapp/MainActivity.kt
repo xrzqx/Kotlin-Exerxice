3d8369fadd6ab85e8369ea6641ef965d5627f6d1
xrzqx <abdrzq84@gmail.com>
2023-07-16 17:57:22 +0700

    add image background

--- HappyBridayApp/app/src/main/java/com/example/happybridayapp/MainActivity.kt
+++ HappyBridayApp/app/src/main/java/com/example/happybridayapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.happybridayapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,10 +62,28 @@ fun GreetingText(message: String,from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        GreetingText(
+            message = message,
+            from = from,
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(8.dp)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HappyBridayAppTheme {
-        GreetingText(message = "Happy Birthday Sam!", from ="From Emma")
+        GreetingImage(message = "Happy Birthday Sam!", from ="From Emma")
     }
 }
