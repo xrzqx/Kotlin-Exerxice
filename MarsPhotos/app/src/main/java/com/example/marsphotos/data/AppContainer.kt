0509d50d97666344fec205005ecd444b521adf65
xrzqx <abdrzq84@gmail.com>
2023-08-12 16:03:51 +0700

    first commit

---
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/data/AppContainer.kt
@@ -0,0 +1,42 @@
+package com.example.marsphotos.data
+
+import com.example.marsphotos.network.MarsApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import kotlinx.serialization.json.Json
+
+interface AppContainer {
+    val marsPhotosRepository: MarsPhotosRepository
+}
+
+/**
+ * Implementation for the Dependency Injection container at the application level.
+ *
+ * Variables are initialized lazily and the same instance is shared across the whole app.
+ */
+class DefaultAppContainer : AppContainer {
+    private val baseUrl = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+    /**
+     * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
+     */
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    /**
+     * Retrofit service object for creating api calls
+     */
+    private val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+
+    /**
+     * DI implementation for Mars photos repository
+     */
+    override val marsPhotosRepository: MarsPhotosRepository by lazy {
+        NetworkMarsPhotosRepository(retrofitService)
+    }
+}
